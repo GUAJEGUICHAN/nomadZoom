8c03f4a62986b86d347653db8aa38706d774cfa4
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-04-03 21:50:46 +0900

    1.6 클라이언트에서 받은 메시지를 그대로 클라이언트에게 보내기

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 //서버
 
+import express from "express";
 import http from "http";
 import WebSocket from "ws";
-import express from "express";
 
 const app = express();
 
@@ -15,24 +15,27 @@ app.get("/*", (_, res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const wss = new WebSocket.Server({server});
+
+function onSocketMessage({socket, message}){
+    // console.log(message)
+    socket.send(message.toString()+" from Server")
+}
 
 //웹소켓이 연결될때 아래 함수 작동
 wss.on("connection", (socket) => {
-  console.log("Connected to Browser ✅");
-  
-  //클라이언트에서 서버가 끊겼을 때 아래 작동
-  socket.on("close", () => console.log("Disconnected from the Browser ❌"));
-  
-  //클라이언트에서 message를 전송했을 때 아래 작동
-  socket.on("message", (message) => {
-    console.log(message.toString());
-  });
-  
-  //서버에서 클라이언트에게 보낸다. 1회성이다.
-  socket.send("hello1");
-  socket.send("hello2");
-  socket.send("hello3");
+    console.log("Connected to Browser ✅");
+
+    //클라이언트에서 서버가 끊겼을 때 아래 작동
+    socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+
+    //클라이언트에서 message를 전송했을 때 아래 작동
+    socket.on("message",(message)=> {onSocketMessage({socket,message})})
+
+    //서버에서 클라이언트에게 보낸다. 1회성이다.
+    socket.send("hello1");
+    socket.send("hello2");
+    socket.send("hello3");
 });
 
 server.listen(3000, handleListen);
