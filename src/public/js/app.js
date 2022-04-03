4859279b8d8687125d35893f81751fa3852993a1
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-04-03 19:04:10 +0900

    웹소켓 프론트 설정

---
+++ src/public/js/app.js
@@ -0,0 +1,20 @@
+//프론트
+
+//아래처럼 주소를 설정해야 모바일에서도 웹소켓 서버 주소를 입력할 수 있다. 
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server ✅");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log("New message: ", message.data);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server ❌");
+});
+
+setTimeout(() => {
+  socket.send("hello from the browser!");
+}, 10000);
